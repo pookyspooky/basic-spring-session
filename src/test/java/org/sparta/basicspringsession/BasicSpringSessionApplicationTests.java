949f95237882e0db507a5c622f5bbe6c815914e7
pookyspooky <union0802@naver.com>
2024-08-21 17:09:57 +0900

    init

---
+++ src/test/java/org/sparta/basicspringsession/BasicSpringSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.basicspringsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
